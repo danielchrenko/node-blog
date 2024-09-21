afb7ce8e3dbf32c2fecf4d47c4dd83418591677e
Daniel Chrenko <danchrenko1@gmail.com>
2024-09-20 17:53:51 -0700

    created express app

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+
+    res.send();
+
+    // # 6 7:07
+    // 
+
+});
